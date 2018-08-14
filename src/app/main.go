1735b819289e5e6ba9d5c1d8b86b152c5c90fbde
Esko Luontola <esko@luontola.fi>
2018-08-14 16:59:45 +0300

    Kubernetes API POC

--- src/app/main.go
+++ src/app/main.go
@@ -5,12 +5,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/davecgh/go-spew/spew"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/dns/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -50,6 +56,42 @@ func main() {
 	// TODO: get the k8s node IPs
 	// TODO: check which DNS records have a different IP
 	// TODO: update DNS records with new IPs
+
+	// https://github.com/kubernetes/client-go/blob/master/examples/in-cluster-client-configuration/main.go
+
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+		// Examples for error handling:
+		// - Use helper functions like e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		_, err = clientset.CoreV1().Pods("default").Get("example-xxxxx", metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod not found\n")
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found pod\n")
+		}
+
+		time.Sleep(10 * time.Second)
+	}
 }
 
 func filterDnsRecordsByName(records []*DnsRecord, names ...string) []*DnsRecord {
