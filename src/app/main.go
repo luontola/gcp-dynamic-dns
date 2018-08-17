d787684184e9ec6405f51331d05b6a4a17825b11
Esko Luontola <esko@luontola.fi>
2018-08-17 15:06:28 +0300

    List cluster nodes

--- src/app/main.go
+++ src/app/main.go
@@ -10,7 +10,6 @@ import (
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/dns/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -73,36 +72,24 @@ func listDns() {
 }
 
 func listNodes() {
-	// https://github.com/kubernetes/client-go/blob/master/examples/in-cluster-client-configuration/main.go
-	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
-	// creates the clientset
+
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	nodes, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-	// Examples for error handling:
-	// - Use helper functions like e.g. errors.IsNotFound()
-	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-	_, err = clientset.CoreV1().Pods("default").Get("example-xxxxx", metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		fmt.Printf("Pod not found\n")
-	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-	} else if err != nil {
-		panic(err.Error())
-	} else {
-		fmt.Printf("Found pod\n")
+	fmt.Printf("There are %d nodes in the cluster\n", len(nodes.Items))
+	for _, node := range nodes.Items {
+		spew.Dump(node)
 	}
 }
 
